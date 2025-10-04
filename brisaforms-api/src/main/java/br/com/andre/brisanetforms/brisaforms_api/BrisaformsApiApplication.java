f11d3f40a833b75d6c771f7e4bc3f8545bbf4997
André Luiz Lessa <49814751+andrelessssa@users.noreply.github.com>
2025-10-04 14:19:39 -0300

    :tada:	init projeto Criado
    :tada:	init projeto Criado

---
+++ brisaforms-api/src/main/java/br/com/andre/brisanetforms/brisaforms_api/BrisaformsApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.andre.brisanetforms.brisaforms_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrisaformsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrisaformsApiApplication.class, args);
+	}
+
+}
